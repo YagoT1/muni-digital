3c9ae796f8023178d6dee318d6bbbb2af644d721
Yago Torres <yagotorres075@gmail.com>
2026-05-22 12:30:04 -0300

    Checkpoint local

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,16 @@
+import './lib/sentry';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import { AccessibilityProvider } from './context/AccessibilityContext'
-import './lib/sentry';
-throw new Error('Sentry frontend test');
-ReactDOM.createRoot(document.getElementById('root')!).render(
+  
+setTimeout(() => {
+  trhow new Error('Sentry frontend test')
+}, 3000);
+
+  ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AccessibilityProvider>
       <BrowserRouter>
